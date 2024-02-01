37fe93c32963ea3026782ab0b6fc895f647f1627
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-01-31 23:19:21 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Titulo from './components/Titulo'
+
+function App() {
+  return (
+    <Titulo frase={'My friend'}></Titulo>
+  )
+}
+
+export default App
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,9 @@
+const Titulo = ({frase}) => {
+    return (
+        <h1>
+            Hello {frase}!
+        </h1>
+    );
+};
+
+export default Titulo;
